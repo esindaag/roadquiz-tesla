6eeb57fdca0249be10ea2a1a4697299fc0bf1894
esindaag <esindag17@gmail.com>
2026-09-13 23:01:21 +0300

    Add Turkey country pack config

---
+++ countries/tr/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  code: 'TR',
+  slug: 'tr',
+  name: 'Turkiye',
+  locale: 'tr-TR',
+  language: 'tr',
+  enabled: true,
+  questionCountTarget: 1000,
+  localQuestionPercent: 65,
+  globalQuestionPercent: 35,
+  questionIdPrefix: 'TR'
+};
